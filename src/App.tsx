b1b93fde6330f53b237bf5e1bef4b5610e66c18a
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-03-25 06:25:55 +0200

    update "resposive layout design"

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,12 @@ import { productList } from "./components/data"
 const renderProductList = productList.map(product => { return <ProductCard key={product.id} product={product}/>});
   const App = () => {
     return (
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 m-5 ">
+      <main className="container">
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 m-5 gap-2 md:gap-4  ">
         {renderProductList}
       </div>
+      </main>
     )
   }
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react"
 import Image from "./Image"
 import Button from "./ui/Button"
 import { IProduct } from "./interfaces"
+import { txtslicer } from "../utils/function";
 
 
 interface IProps {
@@ -9,20 +10,20 @@ product:IProduct;
 }
 
 const ProductCard: FC<IProps> = ({product}) => {
-    const {title,description,imageURL} = product;
+    const {title,description,imageURL,category,price} = product;
     return (
-        <div className="border border-gray-400 p-2 m-2 rounded-md">
-            <Image ImgUrl={imageURL} alt={"Product Name"} className={"rounded-md mb-2"} />
+        <div className="max-w-sm md:max-w-lg mx-auto border border-gray-200 p-2 m-2 rounded-md">
+            <Image ImgUrl={imageURL} alt={"Product Name"} className={"rounded-md mb-2 h-52 w-full lg:object-cover"} />
             <h2>{title}</h2>
-            <p>{description}</p>
+            <p>{txtslicer(description)}</p>
             <div className="flex items-center my-4 space-x-2 ">
                 <span className="w-5 h-5 bg-red-700 rounded-full cursor-pointer" />
                 <span className="w-5 h-5 bg-amber-300 rounded-full cursor-pointer" />
                 <span className="w-5 h-5 bg-green-600 rounded-full cursor-pointer" />
             </div>
             <div className="flex items-center justify-between">
-                <span>500.00$</span>
-                <Image ImgUrl={"https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"} alt={"Product Name"} className={"w-10 h-10 rounded-full"} />
+                <span>{price}$</span>
+                <Image ImgUrl={category.imageURL} alt={category.name} className={"w-10 h-10 rounded-full"} />
             </div>
             <div className="flex items-center justify-between mt-2 space-x-2">
                 <Button className="bg-indigo-600" onClick={()=>{
